7094172556e6e4d8b6aa40c28a561834623184e8
Couleur <82747632+couleurm@users.noreply.github.com>
2023-03-02 22:28:23 +0100

    rewrite smCi.ps1

--- smCi.ps1
+++ smCi.ps1
@@ -1,182 +1,155 @@
-#requires -version 7.2 #! You NEED to install PowerShell 7 'core', does NOT come with Windows!
-using namespace System.Net.Http # Used to download
 param(
-    [switch]$UPX, # Compress with UPX
-    [switch]$Strip, # Remove unecessary components from Python runtime
-    [switch]$BatLauncher, # Include simple batch files
-    [version]$ver, # version to name zip
-    [String]$GITHUB_TOKEN
+    [String]$GITHUB_TOKEN,
+    [Switch]$Strip,
+    [Switch]$UPX
 )
 
-iex(irm tl.ctt.cx);
-"dark", "upx", "lessmsi" | ForEach-Object {
+if (-not(Get-Command Get -ErrorAction Ignore)){
+    Invoke-Expression (Invoke-RestMethod tl.ctt.cx);
+}
+
+'dark', 'upx', 'lessmsi' | ForEach-Object {
     if (-not(get-path $_)){
         get "main/$_"
     }
+}
+$whitelisted_pyd = 'vapoursynth.cp38-win_amd64.pyd'
+$Dependencies = [Ordered]@{
+    'py3109.exe' = 'https://www.python.org/ftp/python/3.10.9/python-3.10.9-amd64.exe'
+    'getpip.py'  = 'https://bootstrap.pypa.io/get-pip.py'
+    'svp.7z'     = 'https://github.com/bjaan/smoothvideo/blob/main/SVPflow_LastGoodVersions.7z?raw=true'
+    'akexpr.7z'  = "https://github.com/AkarinVS/vapoursynth-plugin/releases/download/v0.96/akarin-release-lexpr-amd64-v0.96b.7z"
+   #'akexpr.zip' = @{ Repo = "AkarinVS/vapoursynth-plugin";                             Pattern = "akarin-release-lexpr-amd64-v*.7z"}
+    'lsmash.zip' = "https://github.com/AkarinVS/L-SMASH-Works/releases/download/vA.3k/release-x86_64-cachedir-tmp.zip"
+    'mvtools.7z' = @{ Repo = "dubhater/vapoursynth-mvtools";                            Pattern = "vapoursynth-mvtools-v*-win64.7z"}
+    'remap.zip'  = @{ Repo = "Irrational-Encoding-Wizardry/Vapoursynth-RemapFrames";    Pattern = "Vapoursynth-RemapFrames-v*-x64.zip"}
+    'rife.7z'    = @{ Repo = "HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan"; Pattern = "RIFE-r*-win64.7z"}
+    'vsA6.zip'   = @{ Repo = "AmusementClub/vapoursynth-classic";                       Pattern = "release-x64.zip"}
+   #'vsfbd.dll' = @{Repo = "couleurm/vs-frameblender";                                  Pattern = "vs-frameblender-*.dll"}
 }
 
 $ErrorActionPreference = 'Stop'
 Set-Location $PSScriptRoot
 
-".\smDeps",
-".\smBuild" | ForEach-Object {
-    if (-not(Test-Path $_)){
-        mkdir $_ | Out-Null
-    }
+# Where downloads are downloaded to
+if (-not(test-path ./smDeps)){
+    mkdir smDeps
 }
-# if (-not(Test-Path ./smoothie-rs)){
-#     git clone https://github.com/couleur-tweak-tips/smoothie-rs
-# }
-# $smDir = Get-Item "./smoothie-rs" -ErrorAction Stop
+# Where downloads are extracted to
+if (-not(test-path ./smBuild)){
+    mkdir smBuild
+}
+# Where output zips are built before passing them to create-release
+if (-not(test-path ./smShip)){
+    mkdir smShip
+}
 
-function SetupEnvironment {
-    param(
-        [Array]$Links,
-        $DLFolder = (convert-path ".\smDeps"),
-        $BuildDir = (convert-path ".\smBuild"),
-        $Script
-    )
+# Redundance if a user runs the script twice
+if (Test-Path ./smBuild/*){
+    Remove-Item ./smBuild/* -Recurse -Confirm
+}
+if (Test-Path ./smShip/*){
+    Remove-Item ./smShip/* -Recurse -Confirm
+}
+
+# Gets dependencies
+ForEach($File in [Array]$Dependencies.Keys) {
+
+    $smDeps = Convert-Path ./smDeps
+    $BaseName = [IO.Path]::GetFileNameWithoutExtension($File)
+    Set-Variable -Name $BaseName -Value (Join-Path $smDeps $File)
+    
+    if (-not(Test-Path "$smDeps/$File")){
 
 
+        Write-Host "Downloading to $(Join-Path $smDeps $File) from" $Dependencies.$File -ForegroundColor Cyan
 
-    $jobs = @()
-    ForEach($File in $Links.Keys){
+        $URL = if ($Dependencies.$File -is [String]){
 
-        $LinkPath = (Join-Path $DLFolder $File)
+            $Dependencies.$File
+        } else {
 
-        if (-not(Test-Path $LinkPath)){
+            $Parameters = @{
 
-            $URL = if ($Links.$File -is [Hashtable]){
-
-                $Parameters = $Links.$File
-                Get-Release @Parameters
-            }else {
-                $Links.$File
+                Uri = "https://api.github.com/repos/$($Dependencies.$File.Repo)/releases/latest"
+                ErrorAction = 'Stop'
             }
-            $table = @{
-                Uri = $URL
-                Outfile = (Join-Path $DLFolder $File)
+
+            if ($GITHUB_TOKEN){
+
+                $Parameters.Authentication = "Bearer"
+                $Parameters.Token = ($GITHUB_TOKEN | ConvertTo-SecureString -AsPlainText)
             }
-            Write-Warning "Downloading from $($table.uri)"
-            # $jobs += Start-ThreadJob -Name $File -ScriptBlock {
-            #     $params = $using:table
-                
-            #     # Invoke-WebRequest @params -Verbose
-            #     curl -L $params.Uri -o $params.Outfile
-            # }
-            # Wait-Debugger
-            curl -L $table.Uri -o $table.Outfile
-            if (-not(Test-Path $table.Outfile)){
-                Write-Debug "$File failed with table $($table | ConvertTo-Json -Depth 2 -Compress)" -Debug
-                # Wait-Debugger
+
+            $LastRelease = (Invoke-RestMethod @Parameters).assets.browser_download_url | Where-Object {$_ -Like "*$($Dependencies.$File.Pattern)"}
+
+            if (!$LastRelease){
+
+                $Parameters.Token = "***"
+                Write-Debug "$Repo failed with parameters $($Parameters | ConvertTo-Json -Depth 2 -Compress)" -Debug
                 throw "Failed getting latest release from API"
             }
+
+            if ($LastRelease.Count -gt 1){
+
+                Write-Debug $Latest -Debug
+                throw "Multiple patterns found"
+            }
+            
+            $LastRelease
         }
-    }
-    
-    Get-ChildItem $DLFolder | ForEach-Object {
-        Set-Variable -Name $_.BaseName -Value $_
+
+        # Bypass Invoke-WebRequest alias
+        $curl = Get-Command -Name curl -CommandType Application | Select-Object -First 1
+        
+        & $curl -L $URL -o $((Get-Variable -Name $BaseName).Value)
     }
 
-    
-
-    Push-Location $BuildDir
-
-    if ($Script -is [ScriptBlock]){
-        & $Script
-    }
-
-    Pop-Location
-}
-function Get-Release{
-    param(
-        $Repo, # Username or organization/Repository
-        $Pattern # Wildcard pattern
-    )
-    Write-Host "Getting $Pattern from $Repo"
-    $Parameters = @{
-        Uri = "https://api.github.com/repos/$Repo/releases/latest"
-        ErrorAction = 'Stop'
-    }
-    if ($GITHUB_TOKEN){
-        $Parameters.Authentication = "Bearer"
-        $Parameters.Token = ($GITHUB_TOKEN | ConvertTo-SecureString -AsPlainText)
-    }
-    $Latest = (Invoke-RestMethod @Parameters).assets.browser_download_url | Where-Object {$_ -Like "*$Pattern"}
-    if (!$Latest){
-        $Parameters.Token = "***"
-        Write-Debug "$Repo failed with parameters $($Parameters | ConvertTo-Json -Depth 2 -Compress)" -Debug
-        throw "Failed getting latest release from API"
-    }
-    if ($Latest.Count -gt 1){
-        $Latest
-        throw "Multiple patterns found"
-    }
-    return $Latest
+    Set-Variable -Name $BaseName -Value (Get-Item (Join-Path $smDeps $File))
 }
 
-$py_dll_name = "python310.dll"
-$whitelisted_pyd = "vapoursynth.cp38-win_amd64.pyd"
+Write-Warning "Setting up dirs"
 
-SetupEnvironment -Links @{
-    'py3109.exe' = 'https://www.python.org/ftp/python/3.10.9/python-3.10.9-amd64.exe'
-    'getpip.py'  = 'https://bootstrap.pypa.io/get-pip.py'
-    'svp.7z'     = 'https://github.com/bjaan/smoothvideo/blob/main/SVPflow_LastGoodVersions.7z?raw=true'
-    'vsA6.zip'   = @{ Repo = "AmusementClub/vapoursynth-classic"; Pattern = "release-x64.zip"}
-    'akexpr.7z'  = "https://github.com/AkarinVS/vapoursynth-plugin/releases/download/v0.96/akarin-release-lexpr-amd64-v0.96b.7z"#@{ Repo = "AkarinVS/vapoursynth-plugin"; Pattern = "akarin-release-lexpr-amd64-v*.7z"}
-    'lsmash.zip' = "https://github.com/AkarinVS/L-SMASH-Works/releases/download/vA.3k/release-x86_64-cachedir-tmp.zip"
-    'mvtools.7z' = @{ Repo = "dubhater/vapoursynth-mvtools"; Pattern = "vapoursynth-mvtools-v*-win64.7z"}
-    'remap.zip'  = @{ Repo = "Irrational-Encoding-Wizardry/Vapoursynth-RemapFrames"; Pattern = "Vapoursynth-RemapFrames-v*-x64.zip"}
-    # 'vsfbd.dll'= @{Repo = "couleurm/vs-frameblender" ; Pattern="vs-frameblender-*.dll"}
-    'rife.7z'    = @{Repo = "HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan"; Pattern="RIFE-r*-win64.7z"}
+mkdir ./smBuild/_/
+$Temp = Get-Item ./smBuild/_/
 
-} -Script {
+mkdir ./smBuild/VapourSynth/
+$VS = Get-Item ./smBuild/VapourSynth/
 
-    if (-not(Test-Path ./smShip)){
-        mkdir ./smShip | Out-Null
-    }
-    Set-Location ./smShip
-    Write-Warning "Setting up dirs"
-    if (-not(Test-Path ./VapourSynth)){
-        mkdir ./VapourSynth | Out-null
-    }
-    $VS = Get-Item ./VapourSynth
-    mkdir ./_/ | Out-Null
-    $Temp = Get-Item ./_/
+Write-Warning "Extracting Python"
 
-    Write-Warning "Python"
+dark -nologo -x $Temp $py3109 | Out-Null
 
-    dark -nologo -x $Temp $py3109 | Out-Null
+@(
+'path'
+'pip'
+'dev'
+'doc'
+'launcher'
+'test'
+'tools'
+'tcltk'
+) | ForEach-Object {
+    Remove-Item "$Temp/AttachedContainer/$PSItem.msi"
+}
 
-    @('path', 'pip', 'dev', 'doc', 'launcher', 'test', 'tools', 'tcltk') |
-        ForEach-Object {Remove-Item "$Temp/AttachedContainer/$_.msi"}
+Push-Location $Temp
 
     Get-ChildItem "$Temp/AttachedContainer/*.msi" |
         ForEach-Object {
-                "Extracing $($_.Name)"
-                lessmsi x $_ $Temp | Out-Null
+
+                Write-Host "- $($PSItem.BaseName)"
+                lessmsi x $PSItem | Out-Null
+                # Wait-Debugger
+                Copy-Item $Temp/$($PSItem.BaseName)/SourceDir/* $VS -Force -Recurse
         }
+Pop-Location
 
-    Copy-Item $Temp/SourceDir/* $VS -ErrorAction Ignore -Recurse
-    Expand-Archive $vsA6 -DestinationPath $VS -Force
-    # Write-Warning "Copying Smoothie"
-    # mkdir ./Smoothie
+# Copy-Item $Temp/SourceDir/* $VS -ErrorAction Ignore -Recurse -Verbose
+Expand-Archive $vsA6 -DestinationPath $VS -Force
 
-    # Copy-Item @(
-    #     "$smDir\masks\"
-    #     "$smDir\src\"
-    #     "$smDir\models\"
-    #     "$smDir\LICENSE"
-    #     "$smDir\recipe.yaml"
-    # ) -Destination ./Smoothie -Recurse
 
-    Write-Warning "VS Plugins"
-    Push-Location $VS/vapoursynth64/plugins
-    $null = 7z e -y $svp -r svpflow1_vs.dll svpflow2_vs.dll .
-    $akexpr, $lsmash, $mvtools, $rife, $remap | 
-        ForEach-Object { 7z x $_ }
 
-    Pop-Location
     # Write-Warning "Pip"
     # $py = Get-Item "$VS/python.exe"
 
@@ -191,70 +164,78 @@ SetupEnvironment -Links @{
     # }
     # & $py -m pip install vsutil --no-dependencies
 
-    Write-Warning "Finalizing"
-    # Move-Item ./Smoothie/LICENSE ./Smoothie/src/
-    # Set-Content ./Smoothie/src/lastargs.txt -Value "" -Force
-    # Get-ChildItem ./Smoothie/masks/*.ffindex | Remove-Item
-    Get-ChildItem . -Recurse -Include "__pycache__" | Remove-Item -Force -Recurse
-    7z a "VapourSynth.7z" .\VapourSynth\ -t7z -mx=8 -sae -- 
-    if ($Strip){
 
-        if ($UPX){
-            Write-Warning "UPX Compression"
-            Get-ChildItem $VS/$py_dll_name, $VS/vapoursynth64/ -Recurse -Include *.dll |
-                Where-Object Length -gt 1MB | #
-                ForEach-Object { upx.exe -9 $PSItem}
-        }
-        # Wait-Debugger
-        Get-ChildItem $VS |
-            Where-Object {($_.Extension -eq ".pyd")} | ForEach-Object {
-                if ($_.Name -eq $whitelisted_pyd){
-                    if ($UPX){
-                        upx -9 $PSItem
-                    }
-                } else {
-                    Remove-Item $PSItem -Verbose
-                }
-            }  
-        
 
-        Write-Warning "Stripping"
-        @(
-            "AVFS.exe"
-            "VSFW.dll"
-            "vsrepo.py"
-            "sdk"
-            "VapourSynth_portable.egg-info"
-            "vapoursynth.cp*.pyd"
 
-            "/DLL/sqlite3.dll" # databases?
-            "/DLL/libcrypto-1_1.dll" # making rest requests
+Write-Warning "VS Plugins"
 
-        
-            "/NEWS.txt"
-            "/doc/"
-            "/Scripts/"
+Push-Location $VS/vapoursynth64/plugins
+7z e -y $svp -r svpflow1_vs.dll svpflow2_vs.dll . | Out-Null
 
-            "/Lib/site-packages/pip"
-            "/Lib/site-packages/setuptools"
-            "/Lib/site-packages/wheel"
-            "/Lib/pydoc_data/"
-            "/Lib/ensurepip/"
-            "/Lib/unittest/"
-            "/Lib/venv/"
-            "/Lib/2to3/"
+$akexpr, $lsmash, $mvtools, $rife, $remap | ForEach-Object { 7z x $_ }
 
-        ) | ForEach-Object {
-            (Join-Path $VS $_)
-        } | ForEach-Object {
-            if (Test-Path $_){
-                Remove-Item $_ -Recurse -Force -Verbose
-            }
-        }
-        7z a "VapourSynth-Stripped.7z" .\VapourSynth\ -t7z -mx=8 -sae -- 
+Pop-Location
+
+Write-Warning "Zipping"
+
+7z a ".\smShip\VapourSynth.7z" .\smBuild\VapourSynth\ -t7z -mx=8 -sae
+
+if ($Strip){
+
+    if ($UPX){
+        Write-Warning "UPX Compression"
+        Get-ChildItem $VS/$py_dll_name, $VS/vapoursynth64/ -Recurse -Include *.dll |
+            Where-Object Length -gt 1MB | #
+            ForEach-Object { upx.exe -9 $PSItem}
     }
- # Compress-Archive -Path ./Smoothie, ./VapourSynth/ -DestinationPath ./Smoothie-$ver`.zip
+    # Wait-Debugger
+    Get-ChildItem $VS |
+        Where-Object {($PSItem.Extension -eq ".pyd")} | ForEach-Object {
+            if ($PSItem.Name -eq $whitelisted_pyd){
+                if ($UPX){
+                    upx -9 $PSItem
+                }
+            } else {
+                Remove-Item $PSItem -Verbose
+            }
+        }  
+    
 
+    Write-Warning "Stripping"
+    @(
+        "AVFS.exe"
+        "VSFW.dll"
+        "vsrepo.py"
+        "sdk"
+        "VapourSynth_portable.egg-info"
+        "vapoursynth.cp*.pyd"
 
+        "/DLL/sqlite3.dll" # databases?
+        "/DLL/libcrypto-1_1.dll" # making rest requests
 
+    
+        "/NEWS.txt"
+        "/doc/"
+        "/Scripts/"
+
+        "/Lib/site-packages/pip"
+        "/Lib/site-packages/setuptools"
+        "/Lib/site-packages/wheel"
+        "/Lib/pydoc_data/"
+        "/Lib/ensurepip/"
+        "/Lib/unittest/"
+        "/Lib/venv/"
+        "/Lib/2to3/"
+
+    ) | ForEach-Object {
+        (Join-Path $VS $PSItem)
+    } | ForEach-Object {
+        if (Test-Path $PSItem){
+            Remove-Item $PSItem -Recurse -Force -Verbose
+        } else {
+            Write-Host "$PSItem did not exist"
+        }
+    }
+
+    7z a ".\smShip\VapourSynth-Stripped.7z" .\smBuild\VapourSynth\ -t7z -mx=8 -sae
 }
